fbd6d256ab6776b6b8b45575b9b57f1de88cba4e
Oleg Matveev <matveev@optshintorg.by>
2021-02-14 10:12:12 +0300

    react project make gh-pages for 2nd review  v10.19.rev.2

--- src/components/footer/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer(props) {
-  return(
-    <footer className="footer">
-      <p className="footer__copyright">&copy; 2020. Матвеев Олег</p>
-    </footer>   
-  )
-}
-
-export default Footer;
